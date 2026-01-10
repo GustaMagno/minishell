c963b02b1dba54f513a0806667194c1c9577aa0c
Gustamagno <gumagno2005@gmail.comclear>
2026-01-10 12:32:49 +0000

    echo with -n flag done

--- src/exec/ft_echo.c
+++ src/exec/ft_echo.c
@@ -5,11 +5,13 @@ void	ft_echo(t_cmd *cmd)
 {
 	int	i;
 
-	i = 1;
-	while (cmd->args[i])
+	i = 1 + !(ft_strcmp("-n", cmd->args[1]));
+	while (cmd->args[i + 1])
 	{
 		printf("%s ", cmd->args[i]);
 		i++;
 	}
-	printf("\n");
+	printf("%s", cmd->args[i]);
+	if (ft_strcmp("-n", cmd->args[1]))
+		printf("\n");
 }
